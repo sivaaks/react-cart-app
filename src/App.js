49337b84cfe22c0741e8733736a331f3f8e28d6d
sivaaks <senthilsivaaks@gmail.com>
2021-09-17 23:19:20 +0530

    changes to App.js

--- src/App.js
+++ src/App.js
@@ -96,15 +96,15 @@ class App extends React.Component{
 
   calculateCartTotal=()=>{
     let cartTotal=0;
-    this.state.products.map((product)=>{
-      if(product.inCart) cartTotal+=parseInt(product.price)
+    this.state.products.forEach((product)=>{
+        if(product.inCart) cartTotal+=parseInt(product.price)
     })
     this.setState({cartTotal});
   }
 
   createRatingStars=()=>{
     const products= [...this.state.products];
-    products.map((product)=>{
+    products.forEach((product)=>{
       product['ratingStars']=[];
       for(let i=0;i<product.rating;i++) product['ratingStars'].push(i);
       this.setState({products});
@@ -122,21 +122,21 @@ class App extends React.Component{
   render(){
     return(
       <>
-       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="#!">Start Bootstrap</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#!">All Products</a></li>
-                            <li><hr class="dropdown-divider" /></li>
-                            <li><a class="dropdown-item" href="#!">Popular Items</a></li>
-                            <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+       <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container px-4 px-lg-5">
+            <a className="navbar-brand" href="#!">Start Bootstrap</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                    <li className="nav-item"><a className="nav-link active" aria-current="page" href="/">Home</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/">About</a></li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" id="navbarDropdown" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
+                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a className="dropdown-item" href="/">All Products</a></li>
+                            <li><hr className="dropdown-divider" /></li>
+                            <li><a className="dropdown-item" href="#!">Popular Items</a></li>
+                            <li><a className="dropdown-item" href="#!">New Arrivals</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -175,50 +175,50 @@ class App extends React.Component{
                     <h2>Total: ${this.state.cartTotal}</h2>
                   </Modal.Footer>
                 </Modal>
-                {/* <form class="d-flex"> */}
-                    <button class="btn btn-outline-dark" onClick={()=>{this.showCart()}}>
-                        <i class="bi-cart-fill me-1"></i>
+                {/* <form className="d-flex"> */}
+                    <button className="btn btn-outline-dark" onClick={()=>{this.showCart()}}>
+                        <i className="bi-cart-fill me-1"></i>
                         Cart
-                        <span class="badge bg-dark text-white ms-1 rounded-pill">{this.state.cartCount}</span>
+                        <span className="badge bg-dark text-white ms-1 rounded-pill">{this.state.cartCount}</span>
                     </button>
                 {/* </form> */}
             </div>
         </div>
     </nav>
-    <header class="bg-dark py-5">
-        <div class="container px-4 px-lg-5 my-5">
-            <div class="text-center text-white">
-                <h1 class="display-4 fw-bolder">Shop in style</h1>
-                <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+    <header className="bg-dark py-5">
+        <div className="container px-4 px-lg-5 my-5">
+            <div className="text-center text-white">
+                <h1 className="display-4 fw-bolder">Shop in style</h1>
+                <p className="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
             </div>
         </div>
     </header>
-      <section class="py-5">
-            <div class="container px-4 px-lg-5 mt-5">
-                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+      <section className="py-5">
+            <div className="container px-4 px-lg-5 mt-5">
+                <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                 {this.state.products.map((product)=>{
                   return(
                     <>
-                        <div class="col mb-5">
-                        <div class="card h-100">
-                          {product.onSale? <div class="badge bg-dark text-white position-absolute" style={{top:'0.5rem', right:'0.5rem'}}>Sale</div>:''}
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                             <div class="card-body p-4">
-                                <div class="text-center">
-                                    <h5 class="fw-bolder">{product.name}</h5>
-                                    {product.onSale?<span class="text-muted text-decoration-line-through">$100.00</span>:''}
+                        <div className="col mb-5">
+                        <div className="card h-100">
+                          {product.onSale? <div className="badge bg-dark text-white position-absolute" style={{top:'0.5rem', right:'0.5rem'}}>Sale</div>:''}
+                            <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                             <div className="card-body p-4">
+                                <div className="text-center">
+                                    <h5 className="fw-bolder">{product.name}</h5>
+                                    {product.onSale?<span className="text-muted text-decoration-line-through">$100.00</span>:''}
                                      ${product.price}
                                 </div>
-                                <div class="d-flex justify-content-center small text-warning mb-2">
+                                <div className="d-flex justify-content-center small text-warning mb-2">
                                   {product['ratingStars'].map((eachStar)=>{
                                     return(
-                                      <div class="bi-star-fill"></div>
+                                      <div className="bi-star-fill"></div>
                                     )
                                   })}
                                   </div>
                                 </div>
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"> <button disabled={product.inCart} class="btn btn-outline-dark mt-auto" onClick={()=>{this.handleCart(product.id,'add')}}>{product.inCart?'In cart':'Add to cart'}</button></div>
+                            <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div className="text-center"> <button disabled={product.inCart} className="btn btn-outline-dark mt-auto" onClick={()=>{this.handleCart(product.id,'add')}}>{product.inCart?'In cart':'Add to cart'}</button></div>
                             </div>
                         </div>
                     </div>
